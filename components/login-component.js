7efcaa9ee21f4c361bbeb4adbf8a4944c33e1b74
NastyaTsyf <nastya-cyfarkina@mail.ru>
2023-07-13 21:08:17 +0400

    оживляет форму регистрации

--- components/login-component.js
+++ components/login-component.js
@@ -1,4 +1,4 @@
-import { login } from "../api.js";
+import { loginUser } from "../api.js";
 
 export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }){
     const appHtml = `<h1>Список задач</h1>
@@ -9,22 +9,35 @@ export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }){
         <input type="text" id="login-input" class="input"/>
         <br>
         Пароль
-        <input type="text" id="login-input" class="input"/>
+        <input type="password" id="password-input" class="input"/>
         </div>
         <br />
         <button class="button" id="login-button">Войти</button>
     </div>`
 
     appEl.innerHTML = appHtml;
-
     document.getElementById("login-button").addEventListener('click', () => {
+        const login = document.getElementById("login-input").value
+        const password = document.getElementById("password-input").value
+        if (!login) {
+            alert("Введите логин")
+            return;
+        }
+
+        if (!password) {
+            alert("Введите проль")
+            return;
+        }
+
         setToken("Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k")
-        login({
-            login: "admin",
-            password: "admin"
+        loginUser({
+            login: login,
+            password: password
         }).then((user) => {
             setToken(`Bearer ${user.user.token}`)
             fetchTodosAndRender();
-        });
+        }).catch(error => {
+            alert(error.message)
+        })
     });
 }
